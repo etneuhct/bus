bc93e36fa59f899095def419b160d541525eeac0
jack <jacktchuente@gmail.com>
2018-01-21 08:43:30 -0500

    fix kinect

--- kinect.py
+++ kinect.py
@@ -1,7 +1,17 @@
 from pykinect import nui
 import time
-import json
 
+import requests
+
+url = "https://localhost:3001/api"
+
+def send_data_counter(counter, exercise):
+    data = {"counter": counter, "drill": exercise}
+    requests.post(url, data=data)
+
+def send_data_far(far):
+    data = {"too far": far}
+    requests.post(url, data=data)
 
 def jumping_jacks(min_right_wrist_y, min_left_wrist_y, max_right_wrist_y, max_left_wrist_y, knee_distance_x):
     if max_left_wrist_y - min_left_wrist_y > 1 and max_right_wrist_y - min_right_wrist_y > 1:
@@ -11,82 +21,103 @@ def jumping_jacks(min_right_wrist_y, min_left_wrist_y, max_right_wrist_y, max_le
         return False
 
 
-def touch_toe(wrist_position_y, knee_position_y):
-    return (wrist_position_y - knee_position_y) < -0.5
+def touch_toe(shoulder_position_y):
+    return  shoulder_position_y < 0.15
 
 with nui.Runtime() as kinect:
-    kinect.camera.elevation_angle = 0
-    kinect.skeleton_engine.enable=True
-    start = time.clock()
-    counter_jacks = 0
-    counter_toe = 0
+    while 1:
+        kinect.camera.elevation_angle = 0
+        kinect.skeleton_engine.enable=True
+        start = time.clock()
+        counter_jacks = 0
+        counter_toe = 0
 
-    min_right_wrist_y = 1
-    max_right_wrist_y = -1
-    min_left_wrist_y = 1
-    max_left_wrist_y = -1
+        last_count_toe_date = -1
 
-    knee_distance_x = 0
-    wrist_position_y = 0
-    knee_position_y = -2
+        min_right_wrist_y = 1
+        max_right_wrist_y = -1
+        min_left_wrist_y = 1
+        max_left_wrist_y = -1
 
-    while counter_jacks < 5:
-        frame = kinect.skeleton_engine.get_next_frame()
-        # if time.clock() - start > 5:
-        #    break
-        for skeleton in frame.SkeletonData:
-            if skeleton.eTrackingState == nui.SkeletonTrackingState.TRACKED:
+        knee_distance_x = 0
+        wrist_position_y = 0
+        knee_position_y = -2
+        shoulder_position_y = 2
 
-                positions = skeleton.get_skeleton_positions()
-                """
-                joints = ['Head',' FootRight ',' AnkleRight ',' KneeRight ',' HipRight ',' FootLeft ',' AnkleLeft ',
-                          ' KneeLeft ',' HipLeft ',' HandRight ',' WristRight ',' ElbowRight ',' ShoulderRight ',
-                          ' HandLeft ',' WristLeft ',' ElbowLeft ',' ShoulderLeft ',' Head ',' ShoulderCenter ',
-                          ' Spine ',' HipCenter ']
-                joints = [j.strip() for j in joints]
-                positions = {joint: (position.x, position.y) for joint, position in zip(joints, positions)}
-                #result = json.dumps(positions)
-                """
+        while counter_jacks < 5 or counter_toe < 5:
+            frame = kinect.skeleton_engine.get_next_frame()
+            # if time.clock() - start > 5:
+            #    break
+            for skeleton in frame.SkeletonData:
 
-                # Coordinates of points of interest
-                # Wrists
-                right_wrist_positions_x = positions[nui.JointId.wrist_right].x
-                right_wrist_positions_y = positions[nui.JointId.wrist_right].y
+                if skeleton.eTrackingState != nui.SkeletonTrackingState.TRACKED:
+                    send_data_far(True)
 
-                left_wrist_position_x = positions[nui.JointId.wrist_left].x
-                left_wrist_position_y = positions[nui.JointId.wrist_left].y
+                if skeleton.eTrackingState == nui.SkeletonTrackingState.TRACKED:
+                    send_data_far(False)
+                    positions = skeleton.get_skeleton_positions()
+                    if positions[nui.JointId.head].z > 2:
+                        # Coordinates of points of interest
+                        # Wrists
+                        right_wrist_positions_x = positions[nui.JointId.wrist_right].x
+                        right_wrist_positions_y = positions[nui.JointId.wrist_right].y
 
-                # Knees
-                right_knee_positions_x = positions[nui.JointId.knee_right].x
-                right_knee_positions_y = positions[nui.JointId.knee_right].y
+                        left_wrist_position_x = positions[nui.JointId.wrist_left].x
+                        left_wrist_position_y = positions[nui.JointId.wrist_left].y
 
-                left_knee_positions_x = positions[nui.JointId.knee_left].x
-                left_knee_positions_y = positions[nui.JointId.knee_right].y
+                        # Knees
+                        right_knee_positions_x = positions[nui.JointId.knee_right].x
+                        right_knee_positions_y = positions[nui.JointId.knee_right].y
 
-                knee_distance_x = abs(right_knee_positions_x - left_knee_positions_x)
+                        left_knee_positions_x = positions[nui.JointId.knee_left].x
+                        left_knee_positions_y = positions[nui.JointId.knee_right].y
 
-                if right_wrist_positions_y < min_right_wrist_y and left_wrist_position_y < min_left_wrist_y:
-                    min_right_wrist_y = right_wrist_positions_y
-                    min_left_wrist_y = left_wrist_position_y
-                if right_wrist_positions_y > max_left_wrist_y and left_wrist_position_y > max_left_wrist_y:
-                    max_right_wrist_y = right_wrist_positions_y
-                    max_left_wrist_y = left_wrist_position_y
-                if max_right_wrist_y > 0.5:
-                    if min_left_wrist_y != 1 and max_left_wrist_y != -1:
-                        if jumping_jacks(min_right_wrist_y, min_left_wrist_y, max_right_wrist_y, max_left_wrist_y, knee_distance_x):
-                            min_right_wrist_y = 1
-                            max_right_wrist_y = -1
-                            min_left_wrist_y = 1
-                            max_left_wrist_y = -1
-                            knee_distance_x = 0
-                            counter_jacks += 1
-                            print('Number of jumping jacks: ' + str(counter))
+                        # Shoulders
+                        right_shoulder_position_x = positions[nui.JointId.shoulder_right].x
+                        right_shoulder_position_y = positions[nui.JointId.shoulder_right].y
 
-                wrist_position_y = min(right_wrist_positions_y, left_wrist_position_y)
-                knee_position_y = min(right_knee_positions_y, left_knee_positions_y)
+                        left_shoulder_position_x = positions[nui.JointId.shoulder_left].x
+                        left_shoulder_position_y = positions[nui.JointId.shoulder_left].y
 
-                if touch_toe(wrist_position_y, knee_position_y):
-                    counter_toe += 1
-                    wrist_position_y = 0
-                    knee_position_y = -2
-                    print('You touched your toe: ' + counter_toe + ' time(s).')
+                        shoulder_position_y = min(right_shoulder_position_y, left_shoulder_position_y)
+
+                        #print(str(shoulder_position_y))
+
+
+                        knee_distance_x = abs(right_knee_positions_x - left_knee_positions_x)
+
+                        if right_wrist_positions_y < min_right_wrist_y and left_wrist_position_y < min_left_wrist_y:
+                            min_right_wrist_y = right_wrist_positions_y
+                            min_left_wrist_y = left_wrist_position_y
+
+                        if right_wrist_positions_y > max_left_wrist_y and left_wrist_position_y > max_left_wrist_y:
+                            max_right_wrist_y = right_wrist_positions_y
+                            max_left_wrist_y = left_wrist_position_y
+
+                        if max_right_wrist_y > 0.5 and counter_jacks < 5:
+                            if min_left_wrist_y != 1 and max_left_wrist_y != -1:
+                                if jumping_jacks(min_right_wrist_y, min_left_wrist_y, max_right_wrist_y, max_left_wrist_y, knee_distance_x):
+                                    min_right_wrist_y = 1
+                                    max_right_wrist_y = -1
+                                    min_left_wrist_y = 1
+                                    max_left_wrist_y = -1
+                                    knee_distance_x = 0
+                                    counter_jacks += 1
+                                    send_data_counter(counter_jacks, "jumping_jacks")
+                                    print('Number of jumping jacks: ' + str(counter_jacks))
+
+                        wrist_position_y = min(right_wrist_positions_y, left_wrist_position_y)
+                        knee_position_y = min(right_knee_positions_y, left_knee_positions_y)
+                        subtraction = abs(wrist_position_y-knee_position_y)
+                        #print('Difference y: ' + str(subtraction))
+                        #print('Knee y: ' + str(knee_position_y))
+                        if counter_jacks < 5:
+                            continue
+
+                        if (last_count_toe_date == -1 or time.clock() - last_count_toe_date >= 2) and touch_toe(shoulder_position_y):
+                            last_count_toe_date = time.clock()
+                            counter_toe += 1
+                            shoulder_position_y = 2
+                            send_data_counter(counter_toe, "touch_toe")
+                            print('You touched your toe: ' + str(counter_toe) + ' time(s).')
+
